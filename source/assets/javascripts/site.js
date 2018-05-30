9d71c6a48df5bec7526eeeed0e0672dab6a09e36
naveenb <naveenbhaskar.77@gmail.com>
2018-05-30 15:02:55 +0530

    wip

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
-  $('.tab-nav_item').click(function(){
-    var id = $(this).attr('data-attr');
-    id = ('#'+ id);
-    $('.tab-nav_item').removeClass('active-tab');
-    $('.tab-content').removeClass('active')
-    $(this).addClass('active-tab');
-    $(id).addClass('active');
-  });
+
+  // $('.tab-nav_item').click(function(){
+  //   var id = $(this).attr('data-attr');
+  //   id = ('#'+ id);
+  //   $('.tab-nav_item').removeClass('active-tab');
+  //   $('.tab-content').removeClass('active')
+  //   $(this).addClass('active-tab');
+  //   $(id).addClass('active');
+  // });
   
-  $('.mobile-heading').click(function(){
-    var id = $(this).attr('data-attr');
-    id = ('#'+ id);
-    $(this).toggleClass('open');
-    $('span .fa').removeClass('fa-chevron-down').addClass('fa-chevron-right');
-    $(this).find('span .fa').removeClass('fa-chevron-right').addClass('fa-chevron-down');
-    $(id).toggleClass('active');
-  });
+  // $('.mobile-heading').click(function(){
+  //   var id = $(this).attr('data-attr');
+  //   id = ('#'+ id);
+  //   $(this).toggleClass('open');
+  //   $('span .fa').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+  //   $(this).find('span .fa').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+  //   $(id).toggleClass('active');
+  // });
   
 
     // get started
