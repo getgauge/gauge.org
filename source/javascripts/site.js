182ea096e56083af7face1b5bda3b9a8e33e668f
Chandu199 <n.chandrakanth0@gmail.com>
2018-05-30 12:07:40 +0530

    added js from codepen

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -19,4 +19,32 @@ $(document).ready(function(){
     $(id).toggleClass('active');
   });
   
+
+  
+    $('.tab-nav_item').click(function(){
+      var id = $(this).attr('data-attr');
+      id = ('#'+ id);
+      $('.tab-nav_item').removeClass('active-tab');
+      $('.tab-content').removeClass('active')
+      $(this).addClass('active-tab');
+      $(id).addClass('active');
+    });
+    
+    $('.mobile-heading').click(function(){
+      var id = $(this).attr('data-attr');
+      id = ('#'+ id);
+      $(this).toggleClass('open');
+      
+      var icon = $(this).find('span .fa')  
+    
+      if(icon.hasClass('fa-chevron-down') == true){
+        icon.removeClass('fa-chevron-down').addClass('fa-chevron-right')
+      }else{
+        icon.removeClass('fa-chevron-right').addClass('fa-chevron-down')
+      }
+
+      $(id).toggleClass('active');
+    });
+    
+
 });
