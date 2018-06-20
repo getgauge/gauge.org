c02e33439424aeca081be4cd41c400f052ed510e
sriv <srikanth.ddit@gmail.com>
2018-06-20 11:21:03 -0700

    add Gauge svg badge

--- config.rb
+++ config.rb
@@ -72,6 +72,7 @@ activate :directory_indexes
 page "/feed.xml", layout: false
 page "/sitemap.xml", layout: false
 page "/robots.txt", layout: false
+page "/Gauge_Badge.svg", layout: false
 
 helpers do
   def page_name
