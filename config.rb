54d33efd6497eed6a7ffdbefe7cd04067367d8ad
sriv <srikanth.ddit@gmail.com>
2018-06-20 10:33:11 -0700

    add GA and GTM code

--- config.rb
+++ config.rb
@@ -16,6 +16,7 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 set :base_url, ENV['BASE_URL'] || 'http://localhost:4567'
 set :docs_version, ENV['DOCS_VERSION'] || 'latest'
+set :preview?, ENV['DOCS_VERSION'] == 'master'
 
 set :js_dir, "assets/javascripts"
 set :css_dir, "assets/stylesheets"
